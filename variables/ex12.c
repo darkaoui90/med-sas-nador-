d5c6fc9773257dc81052b649bfaa2f5ff1886045
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2025-09-03 10:45:55 +0200

    exercice 12

--- variables/ex12.c
+++ variables/ex12.c
@@ -1,17 +1,23 @@
-#include <stdio.h> 
-int main()  {
+#include <stdio.h>
 
-    printf("entrer une ")
+int main() {
+    int num;
+    int a,b,c,d;
+    // Write C code here
+    printf("Enter a number with four digits : ");
+    scanf("%d" , &num);
 
-
-
-
-
-
-
-
-
-
+      a=num%10;
+      printf("%d" , a);
+      num= num/10;
+      b=num%10;
+      printf("%d" , b);
+      num=num/10;
+      c=num%10;
+      printf("%d" , c);
+      num=num/10;
+      d=num%10;
+      printf("%d" , d);
 
 
     return 0;
