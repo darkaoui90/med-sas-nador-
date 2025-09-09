446bd27ec6d21035352c8a8f3fdb164180a6a7ed
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2025-09-09 16:02:13 +0200

    updating

--- tableaux/ex11.c
+++ tableaux/ex11.c
@@ -1,31 +1,33 @@
-#include <stdio.h> 
-int main()  {
-    int size;
+#include <stdio.h>
 
-    printf("Enter the size of the array : ");
-    scanf("%d" , &size);
-    int array[size];
+int main() {
+    
+int size, i, replacing, replacement;
 
-    for(int i=0;i<size;i++) {
-        printf("enter number  %d : " , i+1);
-        scanf("%d" , &array[i]);
+ printf(" enter the size of the array : ");
+ scanf("%d", &size);
+
+int array[100];
+
+ printf("emlements of the array :\n");
+    for(i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
     }
 
-    
+ printf("the replacing value : ");
+ scanf("%d", &replacing);
 
+ printf("the new value : ");
+ scanf("%d", &replacement);
+     for(i = 0; i < size; i++) {
+        if(array[i] == replacing) {
+            array[i] = replacement;
+        }
+    }
 
-
-
-
-
-
-
- 
-
-
-
-
-
-
+    printf("the modefied array is :\n");
+    for(i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
     return 0;
 }
