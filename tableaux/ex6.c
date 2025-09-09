61c5994291f4d7e0dbe7613ed1a95f77d2408d0f
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2025-09-09 10:18:48 +0200

    working on day4

---
+++ tableaux/ex6.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int old_array[6]={ 1 , 2 , 3 , 4 , 5 ,6 };
+    int new_array[6];
+    int number;
+    
+    printf("Enter the number of multiplication : ");
+    scanf("%d" , &number);
+    
+    for(int i=0;i<6;i++) {
+        new_array[i]=old_array[i]*number;
+    }
+    printf("The elements of the new array are : ");
+    for(int i=0;i<6;i++) {
+       printf("%d " ,new_array[i]);
+    }
+    return 0;
+}
