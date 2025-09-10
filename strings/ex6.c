eb39ce3b5394f7019fab5b23ccbf543de517f98c
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2025-09-10 18:34:10 +0200

    updating

---
+++ strings/ex6.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <string.h>
+int main()  {
+ char string[10];
+char letter;
+int count=0;
+
+  printf("Enter a string randomly: \n");
+  fgets(string,10,stdin);
+  
+  printf("Enter a letter : ");
+  scanf(" %c" ,&letter );
+
+
+  for(int i=0;i<10;i++)  {
+
+    if(letter==string[i]) {
+        count++;
+    
+    }
+
+
+
+
+  }
+  printf("letter %c exists %d times in the array", letter , count );
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
